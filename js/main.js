b60bd03e8b381cb6e87f5ca5a15a57ca8ec45a8b
AxelCreations <30634177+AxelCreations@users.noreply.github.com>
2017-09-03 13:15:53 -0400

    Resolved Issue #4
    Resolved Issue #4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+;(function(){
+	// setInterval(function(){
+	// 	console.log("Hola");
+	// }, 1000);
+
+})();
+
+
+// INDEX SECTION
+
+function changePromo(currentPromo){
+	$('#promos .promo-selected').removeClass('promo-selected');
+	for (var i = 0; i < 3 ; i++) {
+		$('#promos .promo')[i].style.opacity = '0'
+		$('#promos .promo')[i].style.display = 'none';
+	};
+	$('.'+currentPromo)[0].style.display = 'block'
+	$('.'+currentPromo)[0].style.height = '100%'
+	$('.'+currentPromo)[0].style.opacity = '1'
+};
+
+function promo1(){
+	changePromo("promo1")
+	$('#control1').addClass('promo-selected');
+};
+
+function promo2(){
+	changePromo("promo2")
+	$('#control2').addClass('promo-selected');
+};
+
+function promo3(){
+	changePromo("promo3")
+	$('#control3').addClass('promo-selected');
+};
+
+
+// END OF INDEX SECTION
+
+
+
 function up_quant(){
 	var $quant = document.getElementById("quant");
 	var quant = parseInt($quant.innerHTML) + 1;
@@ -9,3 +50,4 @@ function down_quant(){
 	var quant = parseInt($quant.innerHTML) - 1;
 	$quant.innerHTML = quant;
 };
+
